a358e3b7fcb317681c6e4a461a020fa231e725a3
Pranjal <pranjalgupta1901@gmail.com>
2024-04-23 01:23:02 -0600

    Modified S40network to execute on runtime LCD and client socket working

--- base_external/package/aesd_project/aesd_project.mk
+++ base_external/package/aesd_project/aesd_project.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference lcd display contents
-AESD_PROJECT_VERSION = f6534cd535e1a81197308a60f29d99c00aabba0d
+AESD_PROJECT_VERSION = 2d248ade04396b70c12f3d728a13fe6c02f22f57
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,8 +14,9 @@ AESD_PROJECT_SITE_METHOD = git
 AESD_PROJECT_GIT_SUBMODULES = YES
 
 define AESD_PROJECT_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_display all
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_display all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_test all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client all
 endef
 
 # TODO add your lcd-dislay utilities to the installation steps below
@@ -23,6 +24,7 @@ define AESD_PROJECT_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 0755 $(@D)/lcd_display/* $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/lcd_test/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/client/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
