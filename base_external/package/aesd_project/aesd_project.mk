57596554e7c1dc14094f9aecef6c8209e4fde002
Pranjal <pranjalgupta1901@gmail.com>
2024-04-30 03:57:04 -0600

    added latest commit to pull content

--- base_external/package/aesd_project/aesd_project.mk
+++ base_external/package/aesd_project/aesd_project.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference lcd display contents
-AESD_PROJECT_VERSION = ba1fd158298f7a4598ee1a395e2efb659c4b0375
+AESD_PROJECT_VERSION = 331402e95915fc9f62133ca48c8b1b34607baa22
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,19 +15,19 @@ AESD_PROJECT_GIT_SUBMODULES = YES
 
 define AESD_PROJECT_BUILD_CMDS
 #	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_display all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_test all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd_test all
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket all
 endef
 
 # TODO add your lcd-dislay utilities to the installation steps below
 define AESD_PROJECT_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 0755 $(@D)/lcd_display/* $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/lcd_test/* $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/client/* $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/lcd_test/* $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/client/* $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/socket/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
